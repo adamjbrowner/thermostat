4f1df4d7c9d1fe18d3b03933274dc342d1b48e07
Adam <adamjbrowner@gmail.com>
2016-08-04 11:26:50 +0100

    up to last spec on first thermostat page

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,5 +9,44 @@ describe('Thermostat', function() {
 
   it('Starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20);
-  }); 
+  });
+
+  it('Can be increased with up button', function() {
+    thermostat.increaseTemperature();
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  it('Can be decreased with down button', function() {
+    thermostat.decreaseTemperature();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it("Has a minumum temperature of 10 degress", function() {
+    expect(thermostat.MINIMUM_TEMPERATURE).toEqual(10);
+  });
+
+  it("Can't be reduced below minimum temperature", function() {
+    thermostat.decreaseTemperature(10)
+    thermostat.decreaseTemperature()
+    expect(thermostat.temperature).toEqual(10)
+  });
+
+  it("Has a maximum temperature of 25 degrees if power save mode is on", function() {
+    thermostat.powerSaveOn()
+    expect(thermostat.MAXIMUM_TEMPERATURE).toEqual(25);
+  });
+
+  it('Has a maximum temperature of 32 degrees by power save mode is off', function() {
+    thermostat.powerSaveOff()
+    expect(thermostat.MAXIMUM_TEMPERATURE).toEqual(32);
+  });
+
+  it("Is on power save mode by default", function () {
+    expect(thermostat.powerSave).toEqual(true)
+  });
+
+  it ('Has a reset button which resets temp tp 20', function() {
+    thermostat.reset()
+    expect(thermostat.temperature).toEqual(20);
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,4 +2,35 @@
 
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10
+  this.MAXIMUM_TEMPERATURE = 25
+  this.powerSave = true
+}
+
+Thermostat.prototype.increaseTemperature = function (number = 1) {
+  this.temperature += number
+};
+
+Thermostat.prototype.decreaseTemperature = function (number = 1) {
+  if (this.temperature <= this.MINIMUM_TEMPERATURE) {
+    this.temperature;
+  }
+  else
+  {
+  this.temperature -= number;
+  }
+};
+
+Thermostat.prototype.powerSaveOn = function () {
+  this.powerSave = true
+  this.MAXIMUM_TEMPERATURE = 25
+}
+
+Thermostat.prototype.powerSaveOff = function () {
+  this.powerSave = false
+  this.MAXIMUM_TEMPERATURE = 32
+}
+
+Thermostat.prototype.reset = function () {
+  this.temperature = 20
 }
